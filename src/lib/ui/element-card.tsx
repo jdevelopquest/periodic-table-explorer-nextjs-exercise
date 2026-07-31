e1850ded703795f305e02989b5cfeadeabb4503b
Jacques <212350105+jdevelopquest@users.noreply.github.com>
2026-07-31 13:00:02 +0200

    Edit header of element card

--- src/lib/ui/element-card.tsx
+++ src/lib/ui/element-card.tsx
@@ -2,17 +2,33 @@ import { Element } from "@/lib/types/element";
 
 export function ElementCard({ element }: { element: Element }) {
   return (
-    <div className="flex flex-col items-center gap-2 w-full mx-auto p-3 border border-gray-200 rounded-xl">
-      <span className="text-3xl">{element.symbol}</span>
-      <h4 className="text-center text-2xl mb-4">{element.name}</h4>
-      <div className="w-full flex flex-col gap-2">
-        {Object.entries(element).map(([key, value]) => (
-          <p key={key} className="w-full flex items-center">
-            <span className="text-gray-600 text-sm">{key}:</span>
-            <span className="inline-block ml-auto text-right">{value}</span>
-          </p>
-        ))}
-      </div>
-    </div>
+    <header className="flex flex-col items-center gap-2 w-full mx-auto p-3 border border-gray-200 rounded-xl  bg-gray-100">
+      <p className="flex flex-col items-center gap-2 w-full">
+        <span className="inline-block w-full m-auto text-center text-sm text-gray-600">
+          Atomic number
+        </span>
+        <span className="inline-block w-full m-auto text-center text-3xl">
+          {element.atomicNumber}
+        </span>
+        <span className="inline-block w-full m-auto text-center text-7xl my-2">
+          {element.symbol}
+        </span>
+        <span className="inline-block w-full m-auto text-center text-5xl font-bold my-4">
+          {element.name}
+        </span>
+      </p>
+      <p className="mx-auto flex-col gap-4 md:flex-row">
+        {[element.groupBlock, element.bondingType, element.standardState].map(
+          (value, i) => (
+            <span
+              key={i}
+              className={`block md:inline-block text-center text-sm text-gray-600 rounded-xl bg-gray-50 px-3 m-2 py-2 min-w-24`}
+            >
+              {value}
+            </span>
+          ),
+        )}
+      </p>
+    </header>
   );
 }
