10109f08d823a9d1d3b3c26a3d01f0b096805c0b
Jacques <212350105+jdevelopquest@users.noreply.github.com>
2026-08-04 16:18:35 +0200

    Add metadata homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Periodic Table Explorer",
+  description: "A web app for exploring the periodic table of elements",
+};
+
 export default function Home() {
   return <></>;
 }
